5cb4bc418729a2831f2646a721a6b4b15d2a452c
mwww <115621609+mwww@users.noreply.github.com>
2022-11-28 11:37:34 +0700

    Update generate.py
    make some optimization while keeping all of it in 69 lines of codes.

--- generate.py
+++ generate.py
@@ -1,58 +1,59 @@
 from string import ascii_lowercase, digits
-from itertools import product
+from itertools import product as combiner
 import time
-
 alp_vowels = ['a', 'e', 'i', 'o', 'u']
 
 
-def make_random_words(wl: int, m: str, amt_o_al_re_ch: int, al_re_ch_n_vow: bool, on_al_x_amt_o_re_ch_or_les: bool) \
-        -> list:
+def generate(wl: int, m: str, oar: bool, aouc: int) -> list:
     """
-
     :param wl: word length.
     :param m: materials. (can anyone suggest a better name?)
-    :param amt_o_al_re_ch: amount of allowed repeated chars.
-    :param al_re_ch_n_vow: allow repeated chars not vowel.
-    :param on_al_x_amt_o_re_ch_or_les: only allow x amount of repeated char or less.
+    :param oar: only allow readable.
+    :param aouc: amount of unique chars.
     :return:
     """
+
     r = []
     for length in range(wl, wl + 1):
-        for combo in product(m, repeat=length):
-            (prev_letter, is_exit, combo_wo_duplicates) = ("", False, list(dict.fromkeys(combo)))
-            for current_letter in list(combo):
-                if not prev_letter:
-                    prev_letter = current_letter
-                    continue
-                if prev_letter not in alp_vowels and current_letter not in alp_vowels:
-                    is_exit = True
+        for combo in combiner(m, repeat=length):
+            r.append(''.join(combo))
+    # check for vowels existence
+    if oar:
+        (rt, r) = (r, [])
+        for combo in rt:
+            isCont = False
+            for i in range(1, len(combo)):
+                # if previous letter and current letter is not vowel then skip it. (skip every double consonant)
+                if combo[i-1] not in alp_vowels and combo[i] not in alp_vowels:
+                    isCont = True
                     break
-                prev_letter = current_letter
-            if al_re_ch_n_vow and len(combo_wo_duplicates) <= amt_o_al_re_ch:
-                is_exit = False
-            if is_exit:
+            if isCont:
                 continue
-            if not on_al_x_amt_o_re_ch_or_les:
-                r.append(''.join(combo))
+            r.append(combo)
+    # check for unique chars
+    if aouc:
+        (rt, r) = (r, [])
+        for combo in rt:
+            unique_chars = list(dict.fromkeys(combo))
+            if len(unique_chars) >= aouc:
+                r.append(combo)
     return r
 
 
-def main(conf_og):
-    (confk, conf) = (list(conf_og), list(conf_og.values()))
-    file_name = f"results/result-{conf[0]}-{conf[1]}-{conf[2]}-{conf[3]}-{conf[4]}.txt"
-    print(f"generating {conf[0]} letter(s) words... \n")
-    tic = time.perf_counter()
-    results = make_random_words(conf[0], conf[1], conf[2], conf[3], conf[4])
-    toc = time.perf_counter()
+def main(conf):
+    (confv, output_file_name) = (list(conf.values()), 'result')
+    for c in conf:
+        output_file_name += f"-{conf[c]}"
+    print(f"generating {confv[0]} letter(s) words... \n")
+    (tic, results, toc) = time.perf_counter(), generate(confv[0], confv[1], confv[2], confv[3]), time.perf_counter()
     if len(results) < 1:
-        print("no result found \nreturning...")
+        print(f"no result found \nwasted time: {toc-tic:0.4f} \nreturning...")
         return
-    print(f"Time to Generate {len(results)} words is {toc - tic:0.4f} seconds\noutput to {file_name} \n\nconfigs:")
-    for conf_key in confk:
-        print(f"\t{conf_key} = {conf_og[conf_key]}")
-    with open(file_name, "w") as results_file:
-        for result in results:
-            results_file.write(result + "\n")
+    with open(output_file_name, "w") as results_file:
+        results_file.writelines("\n".join(map(str, results)))
+    print(f"Time to Generate {len(results)} words is {toc-tic:0.4f} seconds\noutput to {output_file_name} \n\nconfigs:")
+    for c in conf:
+        print(f"\t{c} = {conf[c]}")
 
 
 if __name__ == '__main__':
@@ -61,8 +62,7 @@ if __name__ == '__main__':
     settings = {
         "word_length": 4,
         "materials": ascii_lowercase,
-        "amount_of_allowed_repeated_chars": 2,
-        "allow_repeated_chars_not_vowel": False,
-        "only_allow_x_amount_of_repeated_char_or_less": False,
+        "amount_of_unique_chars": 2,
+        "only_allow_readable": True,
     }
     main(settings)
