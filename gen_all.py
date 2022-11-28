d23dbacaa6bc2b846c1c67f494adc064d754bbf3
mwww <115621609+mwww@users.noreply.github.com>
2022-11-28 19:38:11 +0700

    Update gen_all.py

--- gen_all.py
+++ gen_all.py
@@ -3,30 +3,28 @@ from generate import main
 import json
 
 
-def gen_set(wl: int, m: str, amt_o_al_re_ch: int, al_re_ch_n_vow: bool, on_al_x_amt_o_re_ch_or_les: bool):
+def gen_conf(wl: int, m: str, oar: bool, aouc: int):
     return {
         "word_length": wl,
         "materials": m,
-        "amount_of_allowed_repeated_chars": amt_o_al_re_ch,
-        "allow_repeated_chars_not_vowel": al_re_ch_n_vow,
-        "only_allow_x_amount_of_repeated_char_or_less": on_al_x_amt_o_re_ch_or_les,
+        "only_allow_readable": oar,
+        "amount_of_unique_chars": aouc,
     }
 
 
-as_wl = [4, 5, 6, 7, 8, 9]
+as_wl = [2, 3, 4, 5, 6, 7, 8, 9]
+# as_wl = [6, 7, 8, 9]
 as_m = [ascii_lowercase, digits, ascii_lowercase + digits]
-as_amt_o_al_re_ch = [2]
-as_al_re_ch_n_vow = [True, False]
-as_on_al_x_amt_o_re_ch_or_les = [True, False]
+as_oar = [True, False]
+# as_aouc = [0, 1, 2]
+as_aouc = [2]
 
 for cas_wl in as_wl:
     for cas_m in as_m:
-        for cas_amt_o_al_re_ch in as_amt_o_al_re_ch:
-            for cas_al_re_ch_n_vow in as_al_re_ch_n_vow:
-                for cas_on_al_x_amt_o_re_ch_or_les in as_on_al_x_amt_o_re_ch_or_les:
-                    print(f"""\ncurrent: {json.dumps(gen_set(cas_wl, cas_m, cas_amt_o_al_re_ch, cas_al_re_ch_n_vow, 
-                                                           cas_on_al_x_amt_o_re_ch_or_les), indent = 4)}\n""")
-
-                    main(
-                        gen_set(cas_wl, cas_m, cas_amt_o_al_re_ch, cas_al_re_ch_n_vow, cas_on_al_x_amt_o_re_ch_or_les)
-                    )
+        for cas_oar in as_oar:
+            for cas_aouc in as_aouc:
+                conf = gen_conf(cas_wl, cas_m, cas_oar, cas_aouc)
+                print(f"\ncurrent: {json.dumps(conf, indent = 4)}")
+                main(conf)
+                print("\n---")
+                break
